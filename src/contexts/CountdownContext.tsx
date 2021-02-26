bcf4a32de7a4524216ca6f33426680f9e6663e70
Ana Torres <ana.beatriz_19@hotmail.com>
2021-02-26 11:00:57 -0300

    Changes from day 5 - Conclusion and deploy

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -25,7 +25,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengeContext);
 
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
@@ -41,7 +41,7 @@ export function CountdownProvider({ children }: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
     setHasFinished(false);
   }
 
